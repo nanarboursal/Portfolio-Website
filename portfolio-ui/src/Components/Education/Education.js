846a94e7dcccc892be0960bb567ba9362c0de543
Nanar Boursalian <nboursalian@lyft.com>
2023-05-23 12:12:44 -0700

    Adjusted app window and added json files for exp, edu, and skills

--- portfolio-ui/src/Components/Education/Education.js
+++ portfolio-ui/src/Components/Education/Education.js
@@ -4,6 +4,9 @@ export default function Education() {
     return (
         <div id="education" className="edu-block">
             <h1>Education</h1>
+            <div className="edu-info">
+                <h1>Hello</h1>
+            </div>
         </div>
     );
 }
